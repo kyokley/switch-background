1edf313e962275c96cfceef36c652e0b30f3e447
Kevin Yokley <kyokley2@gmail.com>
2019-02-13 21:54:09 -0600

    Move wallpapers to /usr/share/wallpapers dir

--- greeter.sh
+++ greeter.sh
@@ -2,9 +2,9 @@
 
 shopt -s nullglob
 
-WALLPAPER_DIR='/home/yokley/Pictures/wallpapers'
+WALLPAPER_DIR='/usr/share/wallpapers/user'
 WALLPAPERS=($WALLPAPER_DIR/*.jpg)
 
 FILE=${WALLPAPERS[$RANDOM % ${#WALLPAPERS[@]}]}
 
-sed -i -e "s!background=.*!background=$FILE!" /etc/lightdm/slick-greeter.conf
+sed -i -e "s!^background=.*!background=$FILE!" /etc/lightdm/lightdm-gtk-greeter.conf
