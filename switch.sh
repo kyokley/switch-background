f832fd791b3ca052c6611bbad0c911d68e5f49cd
Kevin Yokley <kyokley2@gmail.com>
2019-04-02 11:10:02 -0500

    Handle multiple displays in switch.sh

--- switch.sh
+++ switch.sh
@@ -3,8 +3,10 @@
 shopt -s nullglob
 
 WALLPAPER_DIR='/home/yokley/Pictures/wallpapers'
-WALLPAPERS=($WALLPAPER_DIR/*.jpg)
 
-FILE=${WALLPAPERS[$RANDOM % ${#WALLPAPERS[@]}]}
+NUM_DISPLAYS=$(xrandr | grep connected -w | wc -l)
 
-nitrogen --save --set-scaled "$FILE"
+for i in $(seq 0 $(expr $NUM_DISPLAYS - 1))
+do
+    nitrogen --head=$i --random --save --set-scaled "$WALLPAPER_DIR"
+done
